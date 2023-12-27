e5af18f4db0290ac204ada1f5c3bda3573b6fde9
pengshuai <pengshuai@elelive.tv>
2023-12-27 17:36:32 +0800

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,22 @@
+#coding:utf-8
+import streamlit as st
+
+# 页面设置
+st.set_page_config(
+    page_title = "XX博客",   
+    page_icon = "random",        
+    layout = "centered",         
+    initial_sidebar_state = "auto",  
+)
+
+# 去掉右上角目录按钮
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+st.markdown('xx博客')
